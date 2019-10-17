7a14e0b997e9f82b5a18249b1f5a5ca070b7d276
unknown <alfred.wong@galengrowth.asia>
2019-10-17 21:24:16 +1100

    Enabled connection to azure DB

--- PocoStairDashINSPIRE2019/PocoStairDash/Migrations/Configuration.cs
+++ PocoStairDashINSPIRE2019/PocoStairDash/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace PocoStairDash.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<PocoStairDash.Model1>
+    internal sealed class Configuration : DbMigrationsConfiguration<PocoStairDash.Model2>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(PocoStairDash.Model1 context)
+        protected override void Seed(PocoStairDash.Model2 context)
         {
             //  This method will be called after migrating to the latest version.
 
