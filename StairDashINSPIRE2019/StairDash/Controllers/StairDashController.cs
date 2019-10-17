f8716bfe3094f7ad8fb14362d4162a3c97fc9efc
Samuel Hernandez <samuel.hernandez@bupa.com.au>
2019-10-17 15:50:43 +1100

    initialcommit

---
+++ StairDashINSPIRE2019/StairDash/Controllers/StairDashController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StairDash.Controllers
+{
+    public class StairDashController : Controller
+    {
+        // GET: StairDash
+        public ActionResult Index()
+        {
+            //if (steps == null)
+            //{
+            //    steps = 0;
+            //}
+            //else
+            //{
+            //    ViewBag.steps = steps;
+            //}
+            
+
+            return View();
+        }
+    }
+}
---
+++ StairDashINSPIRE2019/StairDash/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StairDash.Startup))]
+namespace StairDash
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
