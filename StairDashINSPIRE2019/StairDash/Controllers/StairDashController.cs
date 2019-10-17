b87be7eacb0f7d7880ad75a635b945442e04f4be
Samuel Hernandez <samuel.hernandez@bupa.com.au>
2019-10-17 20:31:54 +1100

    attached WCF layer

--- StairDashINSPIRE2019/StairDash/Controllers/StairDashController.cs
+++ StairDashINSPIRE2019/StairDash/Controllers/StairDashController.cs
@@ -4,22 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace StairDash.Controllers
 {
     public class StairDashController : Controller
     {
         // GET: StairDash
-        public ActionResult Index()
+        public ActionResult Index(int? steps)
         {
-            //if (steps == null)
-            //{
-            //    steps = 0;
-            //}
-            //else
-            //{
-            //    ViewBag.steps = steps;
-            //}
+            StairDataServiceReference.ServiceClient srv = new StairDataServiceReference.ServiceClient();
+
             
+
+            int mileStone = 8000;
+            float percent;
+
+            if (steps == null)
+            {
+                steps = 0;
+                ViewBag.steps = steps;
+            }
+            else
+            {
+                ViewBag.steps = steps;
+            }
+
+            ViewBag.stepPercent = percent = (((float)steps / (float)mileStone) * 100.0f);
+            ViewBag.gaugePercent = Math.Round(percent / 5.0) * 5;
+            ViewBag.mileStone = mileStone;
+            ViewBag.teams = srv.GetAllData(0);
 
             return View();
         }
