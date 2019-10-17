70eb53d13e3362dc8eb86e55ec239634691751a5
Alfred Wong <alfred.wong@bupa.com.au>
2019-10-17 16:40:15 +1100

    Added TeamRepo and GetAllTeams method

---
+++ RepoStairDashINSPIRE2019/RepoStairDash/TeamRepo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PocoStairDash;
+
+namespace RepoStairDash
+{
+    public class TeamRepo
+    {
+        Model1 context = new Model1();
+
+        public List<Team> GetAllTeams()
+        {
+            List<Team> retListTeams = new List<Team>();
+
+            foreach (var team in context.Teams)
+            {
+                retListTeams.Add(new Team()
+                {
+                    Id = team.Id,
+                    Name = team.Name,
+                    Score = team.Score
+                });
+            }
+
+            return retListTeams;
+        }
+    }
+}
